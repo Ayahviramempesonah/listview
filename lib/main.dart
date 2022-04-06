fb2eeb3bed9dc7e4a2642253fb7331049ba8ff60
AyahTamvan <bayusatriapurnomo@gmail.com>
2022-04-06 22:01:27 +0700

    revisi dan penambahan fungsi hapus data

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(  const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -10,18 +10,18 @@ class MyApp extends StatefulWidget {
 
   @override
   State<MyApp> createState() => _MyAppState();
-
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Widget>widgets=[];
-
-
+  List<Widget> widgets = [];
+  int counter = 1;
   _MyAppState() {
     for (int i = 0; i < 15; i++)
-      widgets.add(Text('data ke-' + i.toString(),style: TextStyle(fontSize: 55),));
+      widgets.add(Text(
+        'data ke-' + i.toString(),
+        style: TextStyle(fontSize: 55),
+      ));
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +29,41 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           title: Text('my listview flutter'),
-        ),body: ListView(children:widgets,),
+        ),
+        body: ListView(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      widgets.add(Text(
+                        'data ke- ' + counter.toString(),
+                        style: TextStyle(fontSize: 17),
+                      ));
+                      counter++;
+                    });
+                  },
+                  child: Text('tambah data')),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  child: Text('hapus data')),
+              ElevatedButton(onPressed: (){
+                setState(() {
+                  widgets.removeLast();
+                  counter--;
+                });
+              }
+                  , child: Text('hapus bos'))
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: widgets,
+          )
+        ]),
       ),
     );
   }
